c95b7f0847094a34652a2d223f269a5816d58cde
Leobelmont25 <leonardo.braga@alunos.ufersa.edu.br>
2025-10-02 08:55:59 -0300

    Create apb_sequencer.sv

---
+++ apb_sequencer.sv
@@ -0,0 +1,7 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class apb_sequencer extends uvm_sequencer #(apb_seq_item);
+  `uvm_component_utils(apb_sequencer)
+  function new(string name, uvm_component parent); super.new(name,parent); endfunction
+endclass
