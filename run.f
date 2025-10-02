a26ab57556c65252e90104c9900612714bc245b9
Leobelmont25 <leonardo.braga@alunos.ufersa.edu.br>
2025-10-02 09:00:53 -0300

    Create run.f

---
+++ run.f
@@ -0,0 +1,18 @@
+# includes (a partir de dv/CCE_64/run)
++incdir+../../../
++incdir+../../../rtl
++incdir+../../../rtl/common/include
+
+# fontes UVM (sempre com ../UVM/)
+../UVM/apb_if.sv
+#../UVM/axis_s_if.sv
+../UVM/apb_seq_item.sv
+../UVM/apb_sequencer.sv
+../UVM/apb_driver.sv
+../UVM/apb_monitor.sv
+../UVM/apb_agent.sv
+../UVM/apb_scoreboard.sv
+../UVM/env.sv
+../UVM/cfg_seq.sv
+../UVM/cfg_test.sv
+../UVM/top_cfg_tb.sv
